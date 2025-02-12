7fc49150609e5220c4ec4823907cacc134c6c210
jinzhongjia <mail@nvimer.org>
2025-02-12 14:12:07 +0800

    feat: Delayed loading date of dynamic library

--- lua/zig-lamp/ffi.lua
+++ lua/zig-lamp/ffi.lua
@@ -19,17 +19,37 @@ end)())
 
 local _p = path:new(library_path)
 
-local zig_lamp = nil
+--- @type ffi.namespace*|nil|true
+local _zig_lamp = nil
 
-if _p:exists() then
-    zig_lamp = ffi.load(library_path)
+--- @return ffi.namespace*|nil
+local function get_lamp()
+    -- when true, zig_lamp is not found
+    if _zig_lamp == true then
+        return nil
+    end
+    if _zig_lamp then
+        return _zig_lamp
+    end
+    if _p:exists() then
+        _zig_lamp = ffi.load(library_path)
+        return _zig_lamp
+    end
+    _zig_lamp = true
+    return nil
 end
 
+-- check sha256 digest
+--- @param file_path string
+--- @param shasum string
+--- @return boolean
 function M.sha256_digest(file_path, shasum)
-    if zig_lamp == nil then
+    local zig_lamp = get_lamp()
+    if not zig_lamp then
         util.Info("not found zig dynamic library, skip shasum check")
         return true
     end
+    util.Info("try to check shasum")
     return zig_lamp.sha256_digest(file_path, shasum)
 end
 
