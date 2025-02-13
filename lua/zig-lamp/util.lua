3611f5be4ed7add7fe558fdfc02a641a14aebe82
jinzhongjia <mail@nvimer.org>
2025-02-13 23:56:31 +0800

    feat: add `data2zon` method

--- lua/zig-lamp/util.lua
+++ lua/zig-lamp/util.lua
@@ -96,4 +96,43 @@ function M.Warn(message)
     vim.api.nvim_notify(_notify_public_message .. message, vim.log.levels.WARN, {})
 end
 
+local function is_array(t)
+    local i = 0
+    for _ in pairs(t) do
+        i = i + 1
+        if t[i] == nil then
+            return false
+        end
+    end
+    return true
+end
+
+-- convert lua type to a zon string, but now we can not format the string
+function M.data2zon(obj)
+    local res = ""
+    if type(obj) == "table" then
+        res = res .. ".{"
+        local if_arr = is_array(obj)
+        for key, value in pairs(obj) do
+            if not if_arr then
+                res = res .. "." .. key .. "="
+            end
+            res = res .. M.data2zon(value)
+            res = res .. ","
+        end
+        res = res .. "}"
+    elseif type(obj) == "string" then
+        res = string.format('"%s"', obj)
+    elseif type(obj) == "boolean" then
+        if obj then
+            res = "true"
+        else
+            res = "false"
+        end
+    elseif type(obj) == "number" then
+        res = tostring(obj)
+    end
+    return res
+end
+
 return M
